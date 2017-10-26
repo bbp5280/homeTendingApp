e772fe6dd89daf6538f73625a7c28adc1c2ff91b
Ben Porter <bbp5280@gmail.com>
2017-10-26 04:32:22 -0600

    continue setup

--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.css';
+import App from './APP/index';
+import './index.scss';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+
 import rootReducer from './reducers'
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = createStore(rootReducer, devTools) // this is the state inside of my store :D
