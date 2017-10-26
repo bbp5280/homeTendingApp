f3448d46078555a42ba14151609ad7d53c81d83b
Ben Porter <bbp5280@gmail.com>
2017-10-26 05:20:13 -0600

    setup router

--- app/index.js
+++ app/index.js
@@ -2,18 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './APP/index';
 import './index.scss';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
-import rootReducer from './reducers'
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = createStore(rootReducer, devTools) // this is the state inside of my store :D
+import rootReducer from './reducers';
+const devTools =
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-</Provider>,
+  </Provider>,
   document.getElementById('root')
 );
